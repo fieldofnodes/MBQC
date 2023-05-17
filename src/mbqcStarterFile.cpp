f209e3e06fba53c538843a3d3272cef56b7c73f7
Jonathan Miller <jonathan.miller@fieldofnodes.com>
2023-05-17 11:29:12 +0100

    Continued building functionality

--- src/mbqcStarterFile.cpp
+++ src/mbqcStarterFile.cpp
@@ -24,12 +24,27 @@
 #include <boost/array.hpp>
 #include <boost/graph/graph_utility.hpp>
 #include <boost/container/vector.hpp>
+
+// Helper functions
+#include "/home/fieldofnodes/Projects/QuEST/QuEST/projects/MBQC/src/genericHelperFunctions/generalHelperFunctions.hpp"
+#include "/home/fieldofnodes/Projects/QuEST/QuEST/projects/MBQC/src/genericHelperFunctions/writeGraphGraphVizDotFile.hpp"
+
+// Graph functions
 #include "/home/fieldofnodes/Projects/QuEST/QuEST/projects/MBQC/src/graphs/graphConstructions.hpp"
 #include "/home/fieldofnodes/Projects/QuEST/QuEST/projects/MBQC/src/graphs/graphTraversalFlows.hpp"
-#include "/home/fieldofnodes/Projects/QuEST/QuEST/projects/MBQC/src/genericHelperFunctions/generalAssertFunctions.hpp"
-#include "/home/fieldofnodes/Projects/QuEST/QuEST/projects/MBQC/src/testFunctions/graphAssertFunctions.hpp"
-#include "/home/fieldofnodes/Projects/QuEST/QuEST/projects/MBQC/src/genericHelperFunctions/writeGraphGraphVizDotFile.hpp"
+
+// Quantum functions
 #include "/home/fieldofnodes/Projects/QuEST/QuEST/projects/MBQC/src/quantumGraphs/flowMeasurements.hpp"
+#include "/home/fieldofnodes/Projects/QuEST/QuEST/projects/MBQC/src/quantumGraphs/angleConstruction.hpp"
+#include "/home/fieldofnodes/Projects/QuEST/QuEST/projects/MBQC/src/quantumGraphs/quantumGraphClusterStates.hpp"
+
+// Assert functions
+#include "/home/fieldofnodes/Projects/QuEST/QuEST/projects/MBQC/src/testFunctions/generalAssertFunctions.hpp"
+#include "/home/fieldofnodes/Projects/QuEST/QuEST/projects/MBQC/src/testFunctions/graphAssertFunctions.hpp"
+
+
+
+
 
 using namespace boost;
 
@@ -38,150 +53,64 @@ typedef boost::adjacency_list<boost::vecS, boost::vecS,boost::undirectedS> undir
 typedef boost::graph_traits<undirectedGraph>::edge_iterator edgeIterator;
 typedef boost::graph_traits<undirectedGraph>::vertex_iterator vertexIterator;
 
+
+
 // Set up the main function
 int main() {
-  // create graph - lets do a lattice graph
+  // Initialise variable names and types
   int numRows;
   int numCols;
-  int numVertices;
-  
   undirectedGraph latticeGraph;
+  int numVertices;
+  std::vector<qreal> qubitAngles;
+  std::vector<int> measuredQubitsOutcomes;
   
-  
-  numRows = 3;
-  numCols = 6;
-  
-  
-
-  // Create graph (lattice in this case)
+  // Set values to variables
+  numRows = 2;
+  numCols = 3;
   latticeGraph = createLatticeGraph(numRows,numCols);
-
-  std::cout << "The graph has adjacency list:" << std::endl;
+  numVertices = boost::num_vertices(latticeGraph);
   
-  print_graph(latticeGraph);
-  std::cout << std::endl;
-  std::cout << std::endl;
+
+  
+  
+  
   // Print graph to file
   std::string fileDir = "/home/fieldofnodes/Projects/QuEST/QuEST/projects/MBQC/figs";
   std::string graphName = "lattice2DGraph";
   std::string format = "png";
   printGraphToFile(latticeGraph,fileDir,graphName,format);
 
-  //numVertices = boost::num_vertices(graph);
-  numVertices = boost::num_vertices(latticeGraph);
-  
-  // angle list
-  // For initialise all to 0
-  std::vector<qreal> qubitAngles(numVertices,0.0); // input angles in  = {a1,a2,a3,...,an}
-  
-  int numEdges;
 
   // load QuEST environment
   QuESTEnv env = createQuESTEnv();
   
-
-  // set number of qubits
-  int numQubits = numVertices;
   
   // create a quantum register
-  Qureg qureg = createQureg(numQubits, env);
+  Qureg qureg = createQureg(numVertices, env);
   
   // initialise in the plus state
   initPlusState(qureg);
 
-  // entangle qureg according to the graph with CZ gates
-  edgeIterator ei, ei_end;
-  for (boost::tie(ei, ei_end) = edges(latticeGraph); ei != ei_end; ++ei) {
-      auto source = boost::source(*ei, latticeGraph);
-      auto target = boost::target(*ei, latticeGraph);
-      controlledPhaseFlip (qureg,source,target);
-  }
-
+  //qubitAngles = initRandomPiOnNAnglesMod2kPi(numVertices, 4);
+  qubitAngles = initRandomPiOnNAnglesMod2kPiPlusPi(numVertices, 4);
   
-  std::cout << "Vertex being measured: ";
-  // iterate over graph
-  std::vector<int> measuredQubitsOutcomes;
-  undirectedGraph::vertex_iterator vi, vi_end;
-  for (boost::tie(vi, vi_end) = boost::vertices(latticeGraph); vi != vi_end; vi++) {
-      // skip vertices that are in the first column
-      std::cout << *vi << " ";
-      if(*vi < numRows){
-        rotateZ(qureg,*vi, (-1)*qubitAngles[*vi]);
-        hadamard(qureg,*vi);
-        int m = measure(qureg,*vi);
-        measuredQubitsOutcomes.push_back(m);
-      }else if(*vi >= numRows){
-        qreal correctedAngle;
-        correctedAngle = computeCorrectedAngle(latticeGraph,measuredQubitsOutcomes,*vi,numRows,qubitAngles[*vi]);
-        rotateZ(qureg,*vi, (-1)*correctedAngle);
-        hadamard(qureg,*vi);
-        int m = measure(qureg,*vi);
-        measuredQubitsOutcomes.push_back(m);
-      } else {
-        std::cout << "attempting to measure nothing?";
-      }
-  }
+  // entangle graph
+  entangleGraph(qureg,latticeGraph);
+    
+  // measure graph
+  measuredQubitsOutcomes = measureGraph(
+        qureg,
+        latticeGraph,
+        qubitAngles,
+        numRows);
 
-  std::cout << std::endl;
-  std::cout << std::endl;
-  std::cout << "Measured outcome vector has " << measuredQubitsOutcomes.size() << " qubits."  << std::endl; 
-  std::cout << "The measurements are: ";
-  for(int i:measuredQubitsOutcomes){
-    std::cout << i << " ";
-  }
-  std::cout << std::endl;
-  std::cout << std::endl;
+    
 
 
-std::cout << "Measured outcome lattice: " << std::endl;
-  std::vector<std::vector<int>> latticeOutcomes = 
-    returnMeasQubitOutcomesLattice(measuredQubitsOutcomes,numRows,numCols);
+  // print results to screen
+  printResultsToScreen(measuredQubitsOutcomes,numRows,numCols);
 
-  for (const auto& row : latticeOutcomes) {
-            for (int number : row) {
-                std::cout << number << ' ';
-            }
-            std::cout << '\n';
-        }
-
-
-
-  // Random number
-  std::cout << std::endl;
-  std::cout << std::endl;
-  std::cout << "Random numbers: " << std::endl;
-  // Create a random number generator engine
-  std::random_device rd;                   // Obtain a random seed from the hardware
-  std::mt19937 generator(rd());             // Standard mersenne_twister_engine seeded with rd
-
-  // Define the range for the random integer
-  int min = 0;
-  int max = 7;
-
-  // Create a uniform distribution for the range
-  std::uniform_int_distribution<int> distribution(min, max);
-
-  // Generate a random integer
-  std::vector<int> randomNumeratorVector(numVertices);
-  for (int i = 0; i < numVertices-1; i++){
-    int randomInt = distribution(generator);
-    randomNumeratorVector.push_back(randomInt);
-  }
-
-  
-
-  // Print the random integer
-  std::cout << "Random integer: ";
-  for(auto i: randomNumeratorVector){
-    std::cout << i <<  " ";
-  }
-  std::cout << std::endl;
-
-  int total = std::accumulate(randomNumeratorVector.begin(), randomNumeratorVector.end(), 0);
-  int totalMod8 = (total % 8);
-  std::cout << "Total value of from random vector = " << total << std::endl;
-  std::cout << "Total mod 8 = " << totalMod8 << std::endl;
-  std::cout << "Total both values mod 8 = " << ((total+totalMod8) % 8) << std::endl;
 
 
   // unload QuEST
