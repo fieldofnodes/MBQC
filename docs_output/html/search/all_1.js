ff469c1be03d91b2d2a11ac206be85e22400773e
Jonathan Miller <jonathan.miller@fieldofnodes.com>
2023-06-19 18:19:09 +0100

    Updated docs

--- docs_output/html/search/all_1.js
+++ docs_output/html/search/all_1.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['mbqc_0',['MBQC',['../d6/d00/md_src_2archive_2index.html',1,'']]],
+  ['mbqc_0',['MBQC',['../index.html',1,'(Global Namespace)'],['../d6/d00/md_src_2archive_2index.html',1,'(Global Namespace)']]],
   ['mbqc_5finformation_1',['mbqc_information',['../d5/d00/md_src_2archive_2mbqc__information.html',1,'']]]
 ];
