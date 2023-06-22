32256a8a5c6aa4ef04a27164764a6fdd14d21714
Jonathan Miller <jonathan.miller@fieldofnodes.com>
2023-06-22 17:25:17 +0100

    Updated docs

---
+++ build/CMakeFiles/testOddNumberQubitsEverySecondsAngleZero.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testOddNumberQubitsEverySecondsAngleZero.dir/tests/testOddNumberQubitsEverySecondsAngleZero.cpp.o"
+  "CMakeFiles/testOddNumberQubitsEverySecondsAngleZero.dir/tests/testOddNumberQubitsEverySecondsAngleZero.cpp.o.d"
+  "testOddNumberQubitsEverySecondsAngleZero"
+  "testOddNumberQubitsEverySecondsAngleZero.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testOddNumberQubitsEverySecondsAngleZero.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/testTwoQubitsAnglesPiOnTwo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testTwoQubitsAnglesPiOnTwo.dir/tests/testTwoQubitsAnglesPiOnTwo.cpp.o"
+  "CMakeFiles/testTwoQubitsAnglesPiOnTwo.dir/tests/testTwoQubitsAnglesPiOnTwo.cpp.o.d"
+  "testTwoQubitsAnglesPiOnTwo"
+  "testTwoQubitsAnglesPiOnTwo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testTwoQubitsAnglesPiOnTwo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
